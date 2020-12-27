f1202d1b65fbc00e01a888a5668d0fc722e14958
vinylflamingo <fcostoyaprograms@gmail.com>
2020-12-26 22:19:39 -0500

    added responsive menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,32 @@
-console.log('Hello World');
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+header.addEventListener('click', function(){
+    console.log('hamburger clicked.');
+    if(header.classList.contains('open')){
+        //close menu
+        body.classList.remove('noscroll');
+        header.classList.remove('open');
+        fadeElems.forEach(function(element){
+            element.classList.add('fade-out');
+            element.classList.remove('fade-in');
+        });
+        
+    }
+    else { 
+        //open menu
+        body.classList.add('noscroll');
+        header.classList.add('open');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+            
+        });
+        
+
+
+    }
+});
